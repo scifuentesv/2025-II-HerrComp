2ddf0125b3358eb965378496572003cc3e9fc5f0
Sergio Cifuentes <scifuentesv@unal.edu.co>
2025-10-08 18:08:20 -0500

    Added rk4 for armonic oscillator

---
+++ 2025-10-08-rk4/ar_rk4.cpp
@@ -0,0 +1,68 @@
+#include <print>
+#include <valarray>
+#include <string>
+#include <map> // for the parameters
+
+typedef std::valarray<double> state_t; // create a short name to represent the system state type
+typedef std::map<std::string, double> params_t;
+
+void initial_conditions(state_t & s, double t0);
+void print(const state_t & s, double t);
+void fderiv(const state_t & s, state_t & dsdt, double t, params_t & p);
+template <class deriv_t, class s_t, class print_t>
+void integrate_rk4(deriv_t deriv, s_t & s, double tinit, double tend, double dt, params_t & params, print_t writer);
+
+int main(void)
+{
+  int N = 2; // two dimensinal system
+  state_t S(N);
+  params_t params;
+  params["K"] = 0.98765443;
+  params["M"] = 1.142187386;
+  initial_conditions(S, 0);
+
+  integrate_rk4(fderiv, S, 0.0, 10.0, 0.1, params, print);
+  
+  return 0;
+}
+
+
+void fderiv(const state_t & s, state_t & dsdt, double t, params_t & params)
+{
+  dsdt[0] = s[1];
+  dsdt[1] = -params["K"]*s[0]/params["M"];
+}
+
+void initial_conditions(state_t & s, double t0)
+{
+  s[0] = 1.9876;
+  s[1] = 0.0;
+}
+
+void print(const state_t & s, double t)
+{
+  std::println("{:25.16e} {:25.16e} {:25.16e}", t, s[0], s[1]);
+}
+
+template <class deriv_t, class s_t, class print_t>
+void integrate_rk4(deriv_t deriv, s_t & s, double tinit, double tend, double dt, params_t & params, print_t writer)
+{
+  // vector to store derivs
+    s_t k1(s.size()), k2(s.size()), k3(s.size()), k4(s.size());
+
+// time loop
+  for (double t = tinit; t <= tend; t = t + dt) {
+    deriv(s, k1, t, params);
+    s_t s2 = s + (dt * 0.5) * k1;
+    deriv(s2, k2, t + 0.5 * dt, params);
+    s_t s3 = s + (dt * 0.5) * k2;
+    deriv(s3, k3, t + 0.5 * dt, params);
+    s_t s4 = s + dt * k3;
+    deriv(s4, k4, t + dt, params);
+
+    s = s + (dt / 6.0) * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
+
+    // write new state
+    writer(s, t + dt);
+  }
+}
