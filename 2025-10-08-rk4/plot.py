69719a42141cf3a447bccab375a553a03bd49d9a
Sergio Cifuentes <scifuentesv@unal.edu.co>
2025-10-08 18:33:44 -0500

    Added visual aids for axis and title

--- 2025-10-08-rk4/plot.py
+++ 2025-10-08-rk4/plot.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
+from mpl_toolkits.mplot3d import Axes3D 
 
 t,x,y,z = np.loadtxt("data.txt", delimiter=",", unpack=True)
-print(x,y,z)
-fig = plt.figure(figsize=(7, 5))
+fig = plt.figure(figsize=(14, 10))
 ax = fig.add_subplot(111, projection='3d')
-ax.plot(x, y, z, linewidth=0.6)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-ax.set_title(r'Lorenz attractor ($\sigma=10,\ \rho=28,\ \beta=8/3$)')
+ax.plot(x, y, z, linewidth=0.6, color="red")
+ax.set_xlabel('x', fontsize=14)
+ax.set_ylabel('y', fontsize=14)
+ax.set_zlabel('z', fontsize=14)
+ax.set_title(r'Lorentz attractor ($\sigma=10,\ \rho=28,\ \beta=8/3$)', fontsize=18, pad=20)
 fig.savefig("fig.pdf", format="pdf")
